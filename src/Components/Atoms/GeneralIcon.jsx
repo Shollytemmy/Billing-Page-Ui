be7cd6f6b12bcdfec9a179f4c683c445c9fe1f57
Bunyameen <bunyameen@gmail.com>
2023-02-10 12:42:34 +0100

    feat: BillingPage components

---
+++ src/Components/Atoms/GeneralIcon.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function GeralIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      viewBox="0 0 32 32"
+    >
+      <g
+        stroke="#62618F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        clipPath="url(#clip0_3_125)"
+      >
+        <path d="M10.667 10.667h10.666v10.666H10.667V10.667z"></path>
+        <path d="M24 5.333H8A2.667 2.667 0 005.333 8v16A2.667 2.667 0 008 26.667h16A2.667 2.667 0 0026.667 24V8A2.667 2.667 0 0024 5.333z"></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_3_125">
+          <path fill="#fff" d="M0 0H32V32H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default GeralIcon;
--- src/Components/BillingPage.jsx
+++ src/Components/BillingPage.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const BillingPage = () => {
   return (
-    <section className='billing-page'>BillingPage</section>
+    <section className='billing-page'>
+        <aside className='side-bar col-3'>
+            <div className='profile-links'>
+                <div>
+                    <div className='top-profile'>
+                        <img src="/img/lady.png" alt="" />
+                    </div>
+                </div>
+                <div className='bio'>
+                    <strong>Bunyameen</strong>
+                <p className='mail'>@bunyameenn</p>
+
+                </div>
+                
+            </div>
+        </aside>
+    </section>
   )
 }
